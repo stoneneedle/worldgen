6819dae6fd7616d4638e407db64fa941522d2a9d
stoneneedle <stoneneedle@gmail.com>
2022-09-23 20:52:17 -0400

    Made adjustments to hex draw in App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import "./App.css";
 import Sketch from "react-p5";
 
 class App extends React.Component {
-
-  canvasY = 650;
-  canvasX = 650;
-  hexagonSize = 40;
-  numHexesX = 13;
-  numHexesY = 15;
+  canvasY = window.screen.height - 500;
+  canvasX = window.screen.width - 880;
+  hexagonSize = 20;
+  numHexesY = 43;
+  numHexesX = 80;
   gridWidth = this.numHexesX * this.hexagonSize;
   gridHeight = this.numHexesY * this.hexagonSize;
-  startY = 80;
-  startX = 80;
+  startY = 30;
+  startX = 30;
 
   setup = (p5, parentRef) => {
     p5.createCanvas(this.canvasX, this.canvasY).parent(parentRef);
@@ -26,6 +25,7 @@ class App extends React.Component {
       let ar = (p5.PI / 180) * deg;
       p5.vertex(cX + r * p5.cos(ar), cY + r * p5.sin(ar));
     }
+    
     p5.endShape(p5.CLOSE);
   };
 
@@ -33,10 +33,10 @@ class App extends React.Component {
     let count = 0;
     for (let y = this.startY; y < this.gridHeight; y += this.hexagonSize / 1.35) {
       for (let x = this.startX; x < this.gridWidth; x += this.hexagonSize / 1.125) {
-        let offsetX = -2 * (count % 2 == 0);
+        let offsetX = -2 * (count % 2 === 0);
         this.drawHexagonPointyTop(
           p5,
-          x + offsetX + this.hexagonSize * (count % 2 == 0) * 0.5,
+          x + offsetX + this.hexagonSize * (count % 2 === 0) * 0.5,
           y,
           this.hexagonSize / 2
         );
@@ -56,9 +56,9 @@ class App extends React.Component {
   };
 
   render() {
+    console.log(this.canvasX, ' x ', this.canvasY);
     return (
       <div className="App">
-        <h1>react-p5</h1>
         <Sketch setup={this.setup} draw={this.draw} />
       </div>
     );
